01bde776a036bf7819236dd4741fe9210b016f22
sherrydoubleupmarketing <sherry@doubleupmarketing.io>
2024-06-15 00:19:22 +0500

    Adding Email template

--- src/app/emails/EmailTemplate.tsx
+++ src/app/emails/EmailTemplate.tsx
@@ -1,16 +1,5 @@
-import {
-  Body,
-  Container,
-  Head,
-  Heading,
-  Hr,
-  Html,
-  Preview,
-  Tailwind,
-  Text,
-} from "@react-email/components";
+import { Body, Head, Html } from "@react-email/components";
 import * as React from "react";
-import image from "../../../public/Bannerthree.webp";
 
 type MessageUsEmailProps = {
   name: string;
@@ -19,47 +8,131 @@ type MessageUsEmailProps = {
 };
 
 const EmailTemplate = ({ name, email, message }: MessageUsEmailProps) => {
-  const previewText = `Weekly Updates 🚀${name} sent you a message.`;
-
   return (
     <Html>
       <Head />
-      <Preview>{previewText}</Preview>
-      <Tailwind>
-        <Body className="bg-white my-auto mx-auto font-sans">
-          <Container className="my-[20px] mx-auto p-[20px] max-w-4xl">
-            <div className="w-full bg-red-700 flex flex-col items-center justify-center">
-              <h1 className="text-3xl text-bold text-white">ATLANTA</h1>
-              <h1 className="text-5xl text-bold text-white">HAPPENS</h1>
-            </div>
-            <div className="h-96 bg-black w-[90%] m-auto mt-5">
-              <div className="w-[80%] m-auto flex flex-col gap-5">
-                <p className="text-white mt-12">Hasnain Ahmad</p>
-                <hr className="border border-slate-700 w-full -mt-7"></hr>{" "}
-                <p className="text-white">(000) 987 - 876</p>
-                <hr className="border border-slate-700 w-full -mt-7"></hr>{" "}
-                <p className="text-white">doubleupmarketing@gmail.com</p>
-                <hr className="border border-slate-700 w-full -mt-7"></hr>{" "}
-                <p className="text-white">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
-                  omnis aliquam quaerat libero unde cum saepe nemo nisi labore?
-                  Ipsam voluptas temporibus amet quisquam nisi fugiat molestiae
-                  magni enim maxime.
-                </p>
-                <hr className="border border-slate-700 w-full -mt-7"></hr>
-              </div>
-            </div>
-
-            <div className="mt-5 flex items-center justify-center">
-              <img
-                src="https://atlantahappens.vercel.app/_next/image?url=%2FBannertwo.png&w=2048&q=75"
-                alt="Banner image"
-                style={{ width: "auto", height: "100px" }}
-              />
-            </div>
-          </Container>
-        </Body>
-      </Tailwind>
+      <Body
+        style={{
+          backgroundColor: "white",
+          fontFamily: "sans-serif",
+          margin: "auto",
+          maxWidth: "1024px",
+          padding: "20px",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "#dc2626",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "20px 0",
+          }}
+        >
+          <h1
+            style={{
+              color: "white",
+              fontSize: "36px",
+              fontWeight: "bold",
+              margin: "0",
+            }}
+          >
+            ATLANTA
+          </h1>
+          <h1
+            style={{
+              color: "white",
+              fontSize: "48px",
+              fontWeight: "bold",
+              margin: "0",
+            }}
+          >
+            HAPPENS
+          </h1>
+        </div>
+        <div
+          style={{
+            backgroundColor: "black",
+            height: "384px",
+            width: "90%",
+            margin: "20px auto 0 auto",
+          }}
+        >
+          <div
+            style={{
+              width: "80%",
+              margin: "auto",
+              display: "flex",
+              flexDirection: "column",
+              gap: "20px",
+            }}
+          >
+            <p style={{ color: "white", marginTop: "48px", marginBottom: "0" }}>
+              Hasnain Ahmad
+            </p>
+            <hr
+              style={{
+                borderTop: "1px solid #374151",
+                width: "100%",
+                marginTop: "0",
+                marginBottom: "0",
+              }}
+            />
+            <p style={{ color: "white", marginTop: "0", marginBottom: "0" }}>
+              (000) 987 - 876
+            </p>
+            <hr
+              style={{
+                borderTop: "1px solid #374151",
+                width: "100%",
+                marginTop: "0",
+                marginBottom: "0",
+              }}
+            />
+            <p style={{ color: "white", marginTop: "0", marginBottom: "0" }}>
+              doubleupmarketing@gmail.com
+            </p>
+            <hr
+              style={{
+                borderTop: "1px solid #374151",
+                width: "100%",
+                marginTop: "0",
+                marginBottom: "0",
+              }}
+            />
+            <p style={{ color: "white", marginTop: "0", marginBottom: "0" }}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
+              omnis aliquam quaerat libero unde cum saepe nemo nisi labore?
+              Ipsam voluptas temporibus amet quisquam nisi fugiat molestiae
+              magni enim maxime.
+            </p>
+            <hr
+              style={{
+                borderTop: "1px solid #374151",
+                width: "100%",
+                marginTop: "0",
+                marginBottom: "0",
+              }}
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            src="https://atlantahappens.vercel.app/_next/image?url=%2FBannertwo.png&w=2048&q=75"
+            alt="Banner image"
+            style={{ height: "100px", width: "auto" }}
+          />
+        </div>
+      </Body>
     </Html>
   );
 };
