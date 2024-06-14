137b5e4b060c67f6cff586602d3e16f27f9c4597
sherrydoubleupmarketing <sherry@doubleupmarketing.io>
2024-06-15 00:04:17 +0500

    Adding Email template

--- src/app/emails/EmailTemplate.tsx
+++ src/app/emails/EmailTemplate.tsx
@@ -10,6 +10,7 @@ import {
   Text,
 } from "@react-email/components";
 import * as React from "react";
+import image from "../../../public/Bannerthree.webp";
 
 type MessageUsEmailProps = {
   name: string;
@@ -27,29 +28,35 @@ const EmailTemplate = ({ name, email, message }: MessageUsEmailProps) => {
       <Tailwind>
         <Body className="bg-white my-auto mx-auto font-sans">
           <Container className="my-[20px] mx-auto p-[20px] max-w-4xl">
-            <Heading className="text-black text-[20px] font-normal text-left">
-              <strong>Hello {name},</strong>
-            </Heading>
-            <Text className="text-black text-[14px] leading-[24px]">
-              {message}
-            </Text>
-            <Hr className="my-[16px] mx-0 w-full" />
-            <Text className="text-[#666666] text-[12px]">
-              In a world where every email matters, Resend empowers you to send
-              emails that captivate, engage, and convert. It is more than just
-              an email platform; it is a catalyst for modernizing your email
-              communication. Donot settle for outdated email practices. Embrace
-              the future of email communication with Resend and watch your
-              messages soar to new heights.
-            </Text>
-            <Text className="text-[#666666] text-[12px]">
-              This email is delivered to you through the Resend SDK
-              integrations.✨
-            </Text>
-            <Text className="text-[#666666] font-bold text-lg">
-              Best Regards
-            </Text>
-            <Text className="text-[#666666] text-[32px]">Hasnain</Text>
+            <div className="w-full bg-red-700 flex flex-col items-center justify-center">
+              <h1 className="text-3xl text-bold text-white">ATLANTA</h1>
+              <h1 className="text-5xl text-bold text-white">HAPPENS</h1>
+            </div>
+            <div className="h-96 bg-black w-[90%] m-auto mt-5">
+              <div className="w-[80%] m-auto flex flex-col gap-5">
+                <p className="text-white mt-12">Hasnain Ahmad</p>
+                <hr className="border border-slate-700 w-full -mt-7"></hr>{" "}
+                <p className="text-white">(000) 987 - 876</p>
+                <hr className="border border-slate-700 w-full -mt-7"></hr>{" "}
+                <p className="text-white">doubleupmarketing@gmail.com</p>
+                <hr className="border border-slate-700 w-full -mt-7"></hr>{" "}
+                <p className="text-white">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
+                  omnis aliquam quaerat libero unde cum saepe nemo nisi labore?
+                  Ipsam voluptas temporibus amet quisquam nisi fugiat molestiae
+                  magni enim maxime.
+                </p>
+                <hr className="border border-slate-700 w-full -mt-7"></hr>
+              </div>
+            </div>
+
+            <div className="mt-5 flex items-center justify-center">
+              <img
+                src="https://atlantahappens.vercel.app/_next/image?url=%2FBannertwo.png&w=2048&q=75"
+                alt="Banner image"
+                style={{ width: "auto", height: "100px" }}
+              />
+            </div>
           </Container>
         </Body>
       </Tailwind>
