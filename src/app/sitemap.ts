1ddd7ea6e6e3b723b968948c30fe210dca8733ff
sherrydoubleupmarketing <sherry@doubleupmarketing.io>
2024-06-28 01:03:10 +0500

    adding url in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://atlantahappens.vercel.app",
+      url: "https://www.atlantahappens.com/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://atlantahappens.vercel.app/#contactus",
+      url: "https://www.atlantahappens.com/#contactus",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://atlantahappens.vercel.app/#practiceArea",
+      url: "https://www.atlantahappens.com/#practiceArea",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
