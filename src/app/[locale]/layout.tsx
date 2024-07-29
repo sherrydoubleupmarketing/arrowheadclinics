a62fb14532ce02d2bda2ae2d3be7b18702e91821
sherrydoubleupmarketing <sherry@doubleupmarketing.io>
2024-07-29 19:50:04 +0500

    uncomment the G-tag

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -46,7 +46,7 @@ export default async function LocaleLayout({
         </noscript>
         {/* End Google Tag Manager (noscript) */}
         {/* Google Analytics */}
-        {/* <script
+        <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-88B4D9LTQ2"
         ></script>
@@ -57,7 +57,7 @@ export default async function LocaleLayout({
               gtag('js', new Date());
               gtag('config', 'G-88B4D9LTQ2');`,
           }}
-        /> */}
+        />
         {/* End Google Analytics */}
         <NextIntlClientProvider messages={messages}>
           {children}
