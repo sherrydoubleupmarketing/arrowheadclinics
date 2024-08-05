2484886269013475d9ee20aca97977f38151b8a4
sherrydoubleupmarketing <sherry@doubleupmarketing.io>
2024-08-06 02:19:46 +0500

    fix sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.atlantahappens.com/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://www.atlantahappens.com/es",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+  ];
+}
