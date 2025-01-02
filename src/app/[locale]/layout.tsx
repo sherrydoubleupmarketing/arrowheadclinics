5956d879a563662a9adca60abf7809ed3967a62c
sherrydoubleupmarketing <sherry@doubleupmarketing.io>
2025-01-03 00:28:41 +0500

    Updated metadata'

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,28 +6,28 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import Head from "next/head";
 
 export const metadata: Metadata = {
-  title: "Atlanta Car Accident Lawyer | Atlanta Happens",
+  title: "Arrowhead Clinics",
   description:
-    "Atlanta Car Accident Lawyer | We specialize in car accidents & will ensure that you receive maximum compensation for your injuries.",
-  verification: {
-    other: {
-      "ahrefs-site-verification":
-        "cdd50b40aa52a77450bf8d56e9b308a6f3ff9128653aded1396fd1eecbf62d67",
-    },
-  },
+    "Arrowhead Clinics",
+  // verification: {
+  //   other: {
+  //     "ahrefs-site-verification":
+  //       "cdd50b40aa52a77450bf8d56e9b308a6f3ff9128653aded1396fd1eecbf62d67",
+  //   },
+  // },
   openGraph: {
     type: "website",
     url: "https://www.atlantahappens.com/",
     title: "Atlanta Car Accident Lawyer | Atlanta Happens",
     description: "Atlanta Car Accident Lawyer | Atlanta Happens",
-    images: [
-      {
-        url: "https://www.atlantahappens.com/Bannerthree.webp",
-        width: 800,
-        height: 600,
-        alt: "Featured Image",
-      },
-    ],
+    // images: [
+    //   {
+    //     url: "https://www.atlantahappens.com/Bannerthree.webp",
+    //     width: 800,
+    //     height: 600,
+    //     alt: "Featured Image",
+    //   },
+    // ],
   },
 };
 
@@ -41,7 +41,7 @@ export default async function LocaleLayout({
     <html lang={locale}>
       <GoogleTagManager gtmId="GTM-W6HWDQJT" />
       <body>
-        {/* Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript)
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-W6HWDQJT"
@@ -52,7 +52,7 @@ export default async function LocaleLayout({
         </noscript>
         {/* End Google Tag Manager (noscript) */}
         {/* Google Analytics */}
-        <script
+        {/* <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-88B4D9LTQ2"
         ></script>
@@ -63,15 +63,15 @@ export default async function LocaleLayout({
               gtag('js', new Date());
               gtag('config', 'G-88B4D9LTQ2');`,
           }}
-        />
+        /> */}
         {/* End Google Analytics */}
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
-        <Script
+        {/* <Script
           src="//cdn.callrail.com/companies/542677903/715a9313af9e0701b31e/12/swap.js"
           strategy="lazyOnload"
-        />
+        /> */}
       </body>
     </html>
   );
