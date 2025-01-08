8905e04f0d2c20ac099c0c2d807f86b2faa8986f
sherrydoubleupmarketing <sherry@doubleupmarketing.io>
2025-01-08 20:19:31 +0500

    Added sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,23 +2,17 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    // {
-    //   url: "https://www.atlantahappens.com/",
-    //   lastModified: new Date(),
-    //   changeFrequency: "yearly",
-    //   priority: 1,
-    // },
-    // {
-    //   url: "https://www.atlantahappens.com/es",
-    //   lastModified: new Date(),
-    //   changeFrequency: "yearly",
-    //   priority: 1,
-    // },
-    // {
-    //   url: "https://www.atlantahappens.com/icon.ico",
-    //   lastModified: new Date(),
-    //   changeFrequency: "daily",
-    //   priority: 0.7,
-    // }
+    {
+      url: "https://www.arrowhead-clinic.com/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://www.arrowhead-clinic.com/icon.ico",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7,
+    }
   ];
 }
